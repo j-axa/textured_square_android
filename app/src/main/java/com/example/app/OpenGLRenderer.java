7681fb8ee50cc83366ffdbc1bf838bb3aa7fa313
j-axa <josef.axa@gmail.com>
2014-01-31 14:58:52 +0100

    Added fps counter using the new font rendering stuff

--- app/src/main/java/com/example/app/OpenGLRenderer.java
+++ app/src/main/java/com/example/app/OpenGLRenderer.java
@@ -24,11 +24,24 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
     private final String squareFragmentShaderCode;
     private Square square;
 
+    private final Bitmap fontSheetTexture ;
+    private final String fontSheetVertexShaderCode ;
+    private final String fontSheetFragmentShaderCode;
+    private BitmapFont font;
+    private BitmapText fpsText;
+    private int frameCount;
+    private long lastFpsUpdate;
+
     public OpenGLRenderer(final Context context) {
         AssetManager assets = context.getAssets();
         squareTexture = TextureUtils.loadTextureData(assets, "texture/placeholder512.png");
         squareVertexShaderCode = ShaderUtils.loadShader(assets, "shader/simple_vertex.glsl");
         squareFragmentShaderCode = ShaderUtils.loadShader(assets, "shader/simple_fragment.glsl");
+
+        fontSheetTexture = TextureUtils.loadTextureData(assets, "texture/bmpfont.png");
+        fontSheetVertexShaderCode = ShaderUtils.loadShader(assets, "shader/bmpfont_vertex.glsl");
+        fontSheetFragmentShaderCode = ShaderUtils.loadShader(assets, "shader/bmpfont_fragment.glsl");
+
 
         Matrix.setLookAtM(viewMatrix, 0,
                           0f, 0f, -10f, // eye XYZ
@@ -38,24 +51,37 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
     private final float[] projectionMatrix = new float[4 * 4];
     private final float[] viewMatrix = new float[4 * 4];
-    private final float[] modelViewProjectionMatrix = new float[4 * 4];
+    //private final float[] modelViewProjectionMatrix = new float[4 * 4];
 
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
         GLES30.glClearColor(0.3f, 0.3f, 0.7f, 1.0f);
 
         square = new Square(squareTexture, squareVertexShaderCode, squareFragmentShaderCode);
+        final String fontMap = " !*+,-./0123\"456789:;<=#>?@ABCDEFG$HIJKLMNOPQ%RSTUVWXYZ[&\\]^_`'(){|}~";
+        font = new BitmapFont(1152, 16, 16, 16, fontSheetTexture, fontMap, fontSheetVertexShaderCode, fontSheetFragmentShaderCode);
+        fpsText = new BitmapText(font, "");
     }
 
     public void onDrawFrame(GL10 unused) {
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
 
         square.draw(viewMatrix, projectionMatrix);
+
+        frameCount++;
+        if(System.nanoTime() - lastFpsUpdate >= 1000000000) {
+            fpsText = new BitmapText(font, String.format("FPS: %d", frameCount));
+            frameCount = 0;
+            lastFpsUpdate = System.nanoTime();
+        }
+        fpsText.draw();
     }
+
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES30.glViewport(0, 0, width, height);
 
         final float aspectRatio = (float)width / height;
+        //Matrix.orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1, 1, 1, 100);
         Matrix.frustumM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1, 1, 1, 100);
     }
 
