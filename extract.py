4e4c8cfac766a1d3778122ccbbfbb15c9d5e0ffd
j-axa <josef.axa@gmail.com>
2014-02-08 15:56:23 +0100

    Rendering with drawlist draws model OK but UVs incorrect
    Go around the loop_indices and render each triangle instead, this fixes
    UVs

--- extract.py
+++ extract.py
@@ -6,32 +6,27 @@ def extract_mesh(class_name, mesh, path):
             acc = fun(acc, x)
         return acc
     \
-    def java_arr(name, suffix, lst):
+    def java_arr(name, suffix, lst, break_each):
         def _join(a, b, i):
             i[0] += 1
-            fmt = "%s,\n%s" if i[0] % 3 == 0 else "%s,%s"
+            fmt = "%s,\n%s" if i[0] % break_each == 0 else "%s,%s"
             return fmt % (a, b)
         \
         cnt = [0]
-        return "private static final %s[]={\n%s\n};" % (name, rdc(lambda a, b: _join(a, b, cnt), map(lambda x: "%r%s" % (x, suffix), lst)))
-    \
-    def get_data(i_draw, i):
-        def normal(v):
-            return v.normal.to_tuple()
-        \
-        def coord(v):
-            return v.co.to_tuple()
-        \
-        vert = mesh.data.vertices[i_draw]
-        uv = mesh.data.uv_layers.active.data[i].uv
-        \
-        return (i_draw, coord(vert), normal(vert), uv.to_tuple())
+        return "private static final %s[]={\n%s\n};" % (name, rdc(lambda a, b: _join(a, b, cnt), map(lambda x: "%s%s" % (x, suffix), lst)))
     \
     BEGIN_CLASS = "public final class %s extends Model {" % class_name
     END = "}"
     \
-    data = [get_data(i_draw, i) for face in mesh.data.polygons for i_draw, i in zip(face.vertices, face.loop_indices)]
-    data = list(zip(*data))
+    faces = [face for face in mesh.data.polygons]
+    \
+    # go the long way around and render each vertice by the vertex loop index
+    draw_order = [i for f in faces for i in f.loop_indices]
+    \
+    # fetch each triangle separately (am i?)
+    vertices = [c for f in faces for i in f.vertices for c in mesh.data.vertices[i].co]
+    \
+    uvs = [ "{:.6f}".format(x) for uv in mesh.data.uv_layers[0].data for x in uv.uv.to_tuple() ]
     \
     f = open("%s\\%s.java" % (path, class_name), "w")
     \
@@ -40,19 +35,19 @@ def extract_mesh(class_name, mesh, path):
     \
     p(BEGIN_CLASS)
     \
-    p(java_arr("short drawOrder", "", data[0]))
+    p(java_arr("short drawOrder", "", draw_order, 15))
     \
-    for name, data in zip(["float vertices", "float normals", "float uvs"], data[1:]):
-        p(java_arr(name, "f", rdc(lambda a, b: a + b, data)))
+    p(java_arr("float vertices", "f", vertices, 3))
+    \
+    p(java_arr("float uvs", "f", uvs, 6))
     \
     def impl(n, t, v):
         return "@Override\n%s[] %s(){return %s.%s;}\n" % (t, n, class_name, v)
     \
     p(impl("getVertices", "float", "vertices"))
-    p(impl("getNormals", "float", "normals"))
     p(impl("getUvs", "float", "uvs"))
     p(impl("getDrawOrder", "short", "drawOrder"))
-    p("public %s(final Bitmap texture, final String vertexShaderCode, final String fragmentShaderCode) {\nsuper(texture, vertexShaderCode, fragmentShaderCode);\n}" %s class_name)
+    p("public %s(final Bitmap texture, final String vertexShaderCode, final String fragmentShaderCode) {\nsuper(texture, vertexShaderCode, fragmentShaderCode);\n}" % class_name)
     p(END)
     \
     f.close()
